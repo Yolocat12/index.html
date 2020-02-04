5791ff95a1662fa686e045f998a4d000fc6d3454
Yolocat12 <56322815+Yolocat12@users.noreply.github.com>
2020-02-04 14:11:37 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import random
+# ah you must be the guy, yes you! the guy ._. 
+# anyways welcome to my little program over here, the helperinator thing (we seriously ran out of ideas here?)
+# to start are little adventure here, type start() into the console side, and the program will activate itself! W O A H! anyways, feel free to answer the questions it asks you, the program is not sentinet, don't worry, it only uses facts from the internet (at least I think it does?)
+
+def start():
+  print ("welcome to the random helper thing you  have come upon? anyways, you seem to be bored, so I guess I can give you some  random ideas? how about a few quesitons to start out, how bored are you as of right now?'\n")
+  firstquestion()
+
+def firstquestion():
+  print ("(a) isn't this just another one of these rasputin games?\n(b) isn't that the reason I came here?\n(c) just looking for something to do, an ideas? ")
+  
+  first = input()
+  if first == "a": 
+    print ("huh appears like the program had just crashed itself, nice one, guess you can figure out all these errors right?")
+  
+  if first == "b":
+    print ("I mean, there could be multiple other reasons, how about you just type in topic()? we'll see what happens with that")
+ 
+  if first == "c":
+   print ("I got plenty over here, just type topic() to see what selecitons we got today!?")
+
+def topic():
+  print("so I got multiple topics here today, looks like we got ideas for (a) School, (b) Home, and the (c) middle of nowhere? huh")
+
+  topic = input()
+  if topic == "a":
+   print ("well school's got quite a bit of options now doesn't it, we got  (a) move your leg around, (b) talk to somebody, or (c) talk to friends?")
+
+  if topic == "b":
+    print ("why is this even an option, don't you kids have your video games at home, why not just go play some fortnite, or is that dead yet? anyways, here we got some options, which are \n(a) talk to the family, \n(b) go on your phone, \n(c) why not go outside for once? ")
+
+  if topic == "c" :
+    print ("so it doesn't seem like we got anything for 'middle of nowhere', and now the program seems to be in a state of chaos, good job, now were entering a loophole, and now it looks like were crashing, gg no re")  
+
+  answertotopic = input()
+  if answertotopic == "a":
+    print ("well now that were gonna talk to the family, were gonna need some ideas over here, ooo how about these? (a) play a game with the family, (b) discuss life with the family, and finally (c) (SIVA) DATA CORRUPT ")
+  
+  if answertotopic == "b":
+    print ("so what do you kids even have on your phone these days? I mean,these little handheld devices you just look at htem, and then it does stuff right? anyways, here we got (a) play some random game on it, (SIVA) DATA CORRUPT, so which one of those do you want? ")
+
+  SIVACORRUPT = input()
+  if SIVACORRUPT == "a":
+    print ("so what games do you kids like this day to play with the family? board games, some video games? or is it SIVA CORRUPT")
+
+    #WARNING UNKNOWN CORRUPTION DETECTED PLEASE INTIATE CLEANSING PROTCOL WITH ANTICORRUPTION() SIVA INFENSTATION CRITICAL  
+
+def anticorruption():
+  print ("hello and welcome to the helper bot, how may I help you today?")
+  
+
